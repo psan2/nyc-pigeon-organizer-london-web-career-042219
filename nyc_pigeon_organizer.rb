9cfcc284dc5d97149785b8ec9f5e202a70fdaa44
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-04 13:36:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,21 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.map do |attribute, options|
-    binding.pry
     options.map do |option, birds|
       birds.each do |bird|
-        binding.pry
+        if not(pigeon_list.key?(bird))
+          pigeon_list.update(bird => {})
+        end
+        
+        if not(pigeon_list[bird].key?(attribute))
+          pigeon_list[bird].update(attribute => [])
+        end
+
+        if not(pigeon_list[bird][attribute].include?(option))
+          pigeon_list[bird][attribute] << option
+        end
       end
     end
   end
+  return pigeon_list
 end
